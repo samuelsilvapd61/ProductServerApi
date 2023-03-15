b5fbafd93900f5d2578985a6da3d564a2ac753d1
Samuel de Oliveira Silva <samuelsilvapd61@gmail.com>
2023-03-14 23:27:34 -0300

    Configuração incial do projeto

---
+++ src/main/java/com/samuel/product/server/Application.java
@@ -0,0 +1,13 @@
+package com.samuel.product.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
